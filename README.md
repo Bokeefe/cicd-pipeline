0856dd37dd9edac0e9d6b0c3ce38abe3bad33adb
BrendanMatchPoint <brendan.okeefe@wcgclinical.com>
2024-12-26 10:05:05 -0700

    add html index and calls

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 
 This is my first working CI/CD pipeline using AWS EC2 instances + GitHub actions + runner. You can check it here:
 <a href="http://35.161.78.205/" target="_blank">35.161.78.205</a>
+
+
+useful commands
+# docker inspect \
+#   -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' bokeefe96/b-mono-image
+# 172.17.0.2
+
--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
+app.use(cors());
+
 app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/api", (req, res) => {
   res.json({
     status: 200,
     message: "howdy 🤠 world!",
   });
 });
 
+app.listen(80, () => {
+  console.log("Website server running on port 80");
+});
+
 app.listen(8080, () => {
-  console.log("server running on port 8080");
+  console.log("API server running on port 8080");
 });
